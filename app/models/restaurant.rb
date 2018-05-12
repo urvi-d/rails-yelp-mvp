4173667d3329f1a3e30f750fd71512a8ddb152d3
Urvi Doshi <u.n.doshi@gmail.com>
2018-05-12 17:41:13 +0800

    with categories dropdown

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,16 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
+  def self.categories
+     ["Chinese", "Italian", "Japanese", "French", "Belgian", "Other"]
+  end
+
+
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"]}
+  validates :category, inclusion: { in: Restaurant.categories }
 end
 
 # A restaurant must have at least a name and an address.
 # The restaurant category should belong to a fixed list ["chinese", "italian", "japanese", "french", "belgian"].
+# Restaurant::CATEGORIES
